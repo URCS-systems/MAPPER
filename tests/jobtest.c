903c90157a1c34ac166d4481c7436e6c60ac5b32
Prince781 <pferro@u.rochester.edu>
2018-08-16 10:50:04 -0400

    jobtest: Print newline after header in CSV.

--- tests/jobtest.c
+++ tests/jobtest.c
@@ -561,7 +561,7 @@ int test_joblist(const char *filename, FILE *input, FILE *csv, bool write_header
             fprintf(csv, "%s,", jb->name);
         for (struct job *jb = job_list; jb; jb = jb->next)
             fprintf(csv, "%1$s-C/s,%1$s-N/s", jb->name);
-        fprintf(csv, "C,N,runtime,duration");
+        fprintf(csv, "C,N,runtime,duration\n");
     }
 
     for (struct job *jb = job_list; jb; jb = jb->next)
