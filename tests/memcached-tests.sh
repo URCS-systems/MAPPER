e9ad094c3f5e6253abb8cc35469af095f7abc455
Prince781 <pferro@u.rochester.edu>
2018-08-20 14:11:54 -0400

    Adding memcached test script.

---
+++ tests/memcached-tests.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+if [ "$UID" -ne 0 ]; then
+    echo "Run this script as root."
+    exit 1
+fi
+
+while ! pidof memcached &>/dev/null; do
+    sleep 0.1
+done
+
+memcached_parent=($(ps -o ppid= -p $(ps -o ppid= -p $(pidof memcached))))
+memcached_parent=${memcached_parent[0]}
+
+cleanup() {
+    rmdir /var/run/sam/${memcached_parent}
+    rmdir /sys/fs/cgroup/cpuset/sam/app-${memcached_parent}
+    exit 0
+}
+
+trap cleanup TERM EXIT INT QUIT
+
+mkdir -p /sys/fs/cgroup/cpuset/sam/app-${memcached_parent}
+cat /sys/fs/cgroup/cpuset/sam/cpuset.cpus > /sys/fs/cgroup/cpuset/sam/app-${memcached_parent}/cpuset.cpus
+mkdir -p /var/run/sam/${memcached_parent}
+sudo -u $SUDO_USER ./jobtest -n 5 -f workloads/canneal-LSGD.txt
