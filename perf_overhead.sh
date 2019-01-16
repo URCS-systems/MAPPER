0e05c41bd156ce6efabb7b1e196f14fb42c5005c
Prince781 <pferro@u.rochester.edu>
2019-01-16 18:26:32 -0500

    Update perf_overhead script.

--- perf_overhead.sh
+++ perf_overhead.sh
@@ -2,4 +2,4 @@
 
 # feed samd output into this
 
-grep "Elapsed time.*total.*s" | awk '{print $8-$5;sum_amt+=log($5-1) - log($8-1);sum_perf+=log($5-1);sum_total+=log($8-1)}END{print "geomean perf time: " exp(sum_perf/NR); print "geomean iter time: " exp(sum_total/NR); print "geomean perf overhead: " (exp(sum_amt/NR))}'
+grep "Elapsed time.*total.*s" | head -n-1 | awk '{print "iter-perf: " $8-$5;sum_perf+=log($5-1);sum_total+=log($8-1)}END{print "geomean perf time (without sleep): " exp(sum_perf/NR); print "geomean iter time (without sleep): " exp(sum_total/NR); print "geomean perf overhead: " (exp((sum_perf - sum_total)/NR))}'
