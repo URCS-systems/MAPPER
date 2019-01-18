4f4b91daaa6afe7c7747d6b3f42129ff0b4fba0e
Prince781 <pferro@u.rochester.edu>
2019-01-17 21:39:10 -0500

    Fixing scripts and workloads.

--- tests/memcached-tests.sh
+++ tests/memcached-tests.sh
@@ -22,5 +22,23 @@ trap cleanup TERM EXIT INT QUIT
 
 mkdir -p /sys/fs/cgroup/cpuset/sam/app-${memcached_parent}
 cat /sys/fs/cgroup/cpuset/sam/cpuset.cpus > /sys/fs/cgroup/cpuset/sam/app-${memcached_parent}/cpuset.cpus
+if [ "$?" -ne 0 ]; then
+    exit 1
+fi
+
+cat /sys/fs/cgroup/cpuset/sam/cpuset.mems > /sys/fs/cgroup/cpuset/sam/app-${memcached_parent}/cpuset.mems
+if [ "$?" -ne 0 ]; then
+    exit 1
+fi
+
+echo ${memcached_parent} > /sys/fs/cgroup/cpuset/sam/app-${memcached_parent}/tasks
+if [ "$?" -ne 0 ]; then
+    exit 1
+fi
+
 mkdir -p /var/run/sam/${memcached_parent}
-sudo -u $SUDO_USER ./jobtest -n 5 -f workloads/canneal-LSGD.txt
+echo "memcached + ferret w/ SAM-MAP ..."
+sudo -u $SUDO_USER ./jobtest -n 5 -f workloads/ferret.txt
+
+# echo "(Standalone with SAM-MAP) Waiting for SIGINT ..."
+# while true; do sleep 1; done
