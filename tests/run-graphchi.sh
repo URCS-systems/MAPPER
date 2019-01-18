fd8aaf49e5cce90cb54bbf12c777dc5cba9b3c1c
Prince781 <pferro@u.rochester.edu>
2019-01-17 21:51:15 -0500

    Fixing scripts.

--- tests/run-graphchi.sh
+++ tests/run-graphchi.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-source ./include.sh
+GRAPHCHI_DIR=~/Documents/summer2018-research/graphchi-cpp   # change this
+
 cd $GRAPHCHI_DIR
 exec env GRAPHCHI_DIR=$GRAPHCHI_DIR $@
--- tests/run-parsec.sh
+++ tests/run-parsec.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-source ./include.sh
+parsec_dir=~/Documents/summer2018-research/parsec-3.0   # change this
 
 cd $parsec_dir
+source env.sh           # needed for parsecmgmt
 exec $@
